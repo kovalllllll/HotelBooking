1505337e26333bc382d37e1334ccf2f243aea48d
koval <kovalartem9010@gmail.com>
2025-12-18 13:48:11 +0200

    Add Infrastructure layer: DI configuration and migrations

---
+++ src/HotelBooking.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,46 @@
+﻿using HotelBooking.Application.Interfaces;
+using HotelBooking.Application.Services;
+using HotelBooking.Infrastructure.Identity;
+using HotelBooking.Infrastructure.Persistence;
+using HotelBooking.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelBooking.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 6;
+            options.User.RequireUniqueEmail = true;
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IHotelRepository, HotelRepository>();
+        services.AddScoped<IRoomRepository, RoomRepository>();
+        services.AddScoped<IBookingRepository, BookingRepository>();
+        services.AddScoped<IStatisticsRepository, StatisticsRepository>();
+
+        services.AddScoped<IHotelService, HotelService>();
+        services.AddScoped<IRoomService, RoomService>();
+        services.AddScoped<IBookingService, BookingService>();
+
+        return services;
+    }
+}
+
